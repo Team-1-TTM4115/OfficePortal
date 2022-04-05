deee6bec3551016b0c3a45271669c3b734e3d2ac
Rojahno <andreas_sunde97@hotmail.com>
2022-04-05 15:55:19 +0200

    Added more docs

--- src/gui/main_screen.py
+++ src/gui/main_screen.py
@@ -47,7 +47,6 @@ class Screen:
         welcome_text.destroy()
         startup_screen.destroy()
 
-
     def create_main_screen(self) -> tk.Tk:
         """
         Sets up the main screen for the magic mirror. Used
@@ -66,8 +65,13 @@ class Screen:
         self.show_frame("start_frame")
         return root
 
-    def create_start_page(self, root):
-        start_frame = tk.Frame(root, bg='black')
+    def create_start_page(self, parent_frame: tk.Frame) -> None:
+        """
+        Creates and adds the different modules to the start page.
+        :param parent_frame The parent frame
+        :return: None
+        """
+        start_frame = tk.Frame(parent_frame, bg='black')
         start_frame.pack(expand=False, fill=BOTH, side=TOP, anchor=CENTER)
         # Creates the clock object on the main screen.
         clock = Clock(start_frame)
@@ -82,7 +86,12 @@ class Screen:
         self.frames['start_frame'] = start_frame
         start_frame.grid(row=0, column=0, sticky="nsew")
 
-    def create_grid_frame(self, root):
+    def create_grid_frame(self, root) -> tk.Frame:
+        """
+        Creates the grid that keeps the frames.
+        :param root: The root window
+        :return: None
+        """
         frame_container = tk.Frame(root)
         frame_container.pack(side="top", fill="both", expand=True)
         frame_container.grid_rowconfigure(0, weight=1)
@@ -97,8 +106,12 @@ class Screen:
         self.frames['test_page'] = test_frame
         test_frame.grid(row=0, column=0, sticky="nsew", )
 
-    def show_frame(self, frame_name):
-        '''Show a frame for the given page name'''
+    def show_frame(self, frame_name) -> None:
+        """
+        Show a frame for the given page name
+        :param frame_name: The name you want to show.
+        :return: None
+        """
         frame = self.frames[frame_name]
         print(frame)
         frame.tkraise()
@@ -111,7 +124,8 @@ class Screen:
     def run(self) -> None:
         """
         Runs the program and calls the tkinter mainloop function.
-        Nb: Any code written below the mainloop function will not be executed.
+        Nb: Any code written below the mainloop function will not be
+        executed.
         :return: None
         """
         self.configure_startup_screen()
