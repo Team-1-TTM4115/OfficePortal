74efb5eee8c38de9dddf507bee034eb393ea763f
Ingebret Oseland <Ingebretoseland@hotmail.com>
2022-05-04 12:43:07 +0200

    fiks i gui_stm noe som ikke var likt som state machine diagramet, men som hadde ingen funskjollell funksjon

--- src/gui/gui_controller.py
+++ src/gui/gui_controller.py
@@ -16,14 +16,8 @@ class GuiController:
         self.screen.create_qr_page()
         self.screen.show_frame('qr_frame')
 
-    def leave_qr_scanner(self):
-        self.screen.destroy_qr_page()
-
     def enter_gallery_mode(self):
         self.screen.show_frame('start_frame')
-
-    def leave_gallery_mode(self):
-        pass
 
     def enter_waiting_mode(self):
         self.screen.create_waiting_page()
@@ -33,9 +27,6 @@ class GuiController:
 
     def enter_video_call(self):
         self.screen.create_video_page()
-
-    def leave_video_call(self):
-        pass
 
     def show_filter_menu(self):
         self.screen.show_filter()
@@ -61,34 +52,25 @@ class GuiController:
               "effect": "enter_waiting_mode()"}
         t5 = {"trigger": "open_menu", "source": "video_call", "target": "menu_open", "effect": "show_filter_menu()"}
         t6 = {"trigger": "close_menu", "source": "menu_open", "target": "video_call", "effect": "close_filter_menu()"}
-        t7 = {"trigger": "selected_face_filter", "source": "menu_open", "target": "video_call",
-              "effect": "apply_face_filter()"}
-        t8 = {"trigger": "selected_background_filter", "source": "menu_open", "target": "video_call",
-              "effect": "apply_background_filter()"}
-        t9 = {"trigger": "enter_gallery_mode", "source": "qr_scanner", "target": "gallery_mode",
+        t7 = {"trigger": "enter_gallery_mode", "source": "qr_scanner", "target": "gallery_mode",
               "effect": "enter_gallery_mode()"}
-        t10 = {"trigger": "enter_gallery_mode", "source": "waiting_mode", "target": "gallery_mode",
+        t8 = {"trigger": "enter_gallery_mode", "source": "waiting_mode", "target": "gallery_mode",
                "effect": "enter_gallery_mode()"}
-        t11 = {"trigger": "enter_gallery_mode", "source": "video_call", "target": "gallery_mode",
+        t9 = {"trigger": "enter_gallery_mode", "source": "video_call", "target": "gallery_mode",
                "effect": "enter_gallery_mode()"}
-        t12 = {"trigger": "enter_qr_scanner", "source": "video_call", "target": "qr_scanner",
+        t10 = {"trigger": "enter_qr_scanner", "source": "video_call", "target": "qr_scanner",
                "effect": "enter_qr_scanner()"}
-        t13 = {"trigger": "enter_waiting_mode", "source": "video_call", "target": "waiting_mode",
+        t11 = {"trigger": "enter_waiting_mode", "source": "video_call", "target": "waiting_mode",
                "effect": "enter_waiting_mode()"}
-        t14 = {"trigger": "enter_qr_scanner", "source": "waiting_mode", "target": "qr_scanner",
+        t12 = {"trigger": "enter_qr_scanner", "source": "waiting_mode", "target": "qr_scanner",
                "effect": "enter_qr_scanner()"}
+        t13 = {"trigger": "enter_video_call", "source": "qr_scanner", "target": "video_call",
+              "effect": "enter_video_call()"}
 
-        # States
-        s0 = {"name": "initial"}
-        s1 = {"name": "gallery_mode"}
-        s2 = {"name": "qr_scanner"}
-        s3 = {"name": "waiting_mode"}
-        s4 = {"name": "video_call", "entry": "new_frame(*)"}
-        s5 = {"name": "menu_open"}
 
         machine = stmpy.Machine(name="gui_stm",
-                                transitions=[t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14],
-                                states=[s0, s1, s2, s3, s4, s5], obj=gui_stm)
+                                transitions=[t0, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12,t13],
+                                 obj=gui_stm)
 
         gui_stm.stm = machine
 
--- src/gui/gui_stm.py
+++ src/gui/gui_stm.py
@@ -13,32 +13,17 @@ class GuiSTM:
     def send_err_msg(self, e_msg):
         self.gui_controller.send_err_msg(e_msg)
 
-    def new_frame(self, frame=None):
-        pass
-
     def enter_qr_scanner(self):
         self.gui_controller.enter_qr_scanner()
-
-    def leave_qr_scanner(self):
-        self.gui_controller.leave_qr_scanner()
 
     def enter_gallery_mode(self):
         self.gui_controller.enter_gallery_mode()
 
-    def leave_gallery_mode(self):
-        self.gui_controller.leave_gallery_mode()
-
     def enter_waiting_mode(self):
         self.gui_controller.enter_waiting_mode()
 
-    def leave_waiting_screen(self):
-        self.gui_controller.leave_waiting_screen()
-
     def enter_video_call(self):
         self.gui_controller.enter_video_call()
-
-    def leave_video_call(self):
-        self.gui_controller.leave_video_call()
 
     def show_filter_menu(self):
         self.gui_controller.show_filter_menu()
